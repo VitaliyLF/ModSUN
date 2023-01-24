515315f371f305893f33b4f3daf45c953a383d14
Vetosy <xigga11@gmail.com>
2023-01-24 18:54:38 +0400

    added adaptiv header footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,13 @@
 $(function () {
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active')
+  })
+
+  $('.footer__hidden').on('click', function () {
+    $(this).next().slideToggle()
+    $(this).toggleClass('footer-top__title--active')
+  })
+
   $('.blog-page__slider').slick({
     prevArrow:
       '<button type="button" class="slick-prev"><svg fill="#000000" width="20px" height="20px" viewBox="-128 0 512 512" xmlns="http://www.w3.org/2000/svg"><g stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></g></svg></button>',
@@ -12,7 +21,9 @@ $(function () {
     $('.product-tabs__top-item').removeClass('product-tabs__top-item--active')
     $(this).addClass('product-tabs__top-item--active')
 
-    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active')
+    $('.product-tabs__content-item').removeClass(
+      'product-tabs__content-item--active'
+    )
     $($(this).attr('href')).addClass('product-tabs__content-item--active')
   })
 
@@ -32,7 +43,9 @@ $(function () {
   })
 
   $('.shop-content__filter-btn').on('click', function () {
-    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active')
+    $('.shop-content__filter-btn').removeClass(
+      'shop-content__filter-btn--active'
+    )
     $(this).addClass('shop-content__filter-btn--active')
   })
 
@@ -122,6 +135,4 @@ $(function () {
   initializeClock('promo__clock', deadline)
 
   let map
-
-
 })
